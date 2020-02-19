c3c5a64bfe786ebc409ac33a8a71208500454862
Iury Vasconcelos <iury0393@gmail.com>
2020-02-19 16:17:42 -0300

    Delegando as redes

--- app/sender.py
+++ app/sender.py
@@ -1,9 +1,27 @@
+import psycopg2
 from bottle import route, run, request
-     @route('/', method='POST')
-     def send():
-assunto = request.forms.get('assunto')
-mensagem = request.forms.get('mensagem')
-return 'Mensagem enfileirada ! Assunto: {} Mensagem: {}'.format(
-             assunto, mensagem)
+
+DSN = 'dbname=email_sender user=postgres host=db'
+SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
+
+def register_message(assunto, mensagem): 
+     conn = psycopg2.connect(DSN)
+     cur = conn.cursor()
+     cur.execute(SQL, (assunto, mensagem)) 
+     conn.commit()
+     cur.close() 
+     conn.close()
+
+     print('Mensagem registrada !')
+
+@route('/', method='POST')
+def send():
+     assunto = request.forms.get('assunto')
+     mensagem = request.forms.get('mensagem') 
+     
+     register_message(assunto, mensagem)
+     return 'Mensagem enfileirada ! Assunto: {} Mensagem: {}'.format(
+          assunto, mensagem)
+
 if __name__ == '__main__':
-run(host='0.0.0.0', port=8080, debug=True)
+     run(host='0.0.0.0', port=8080, debug=True)
